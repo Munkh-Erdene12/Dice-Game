e647d3f7122bf694fb1c4f8d732751e2a35ba724
Munkh_Erdene <munkherdene.n1207@gmail.com>
2019-11-24 04:52:44 -0800

    program duusaw

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
  *********************************************************************************/
 
 var activePlayer = 0;
-
+//тоглоом дуусан эсэхийг хадаглах төлвийн хувьсагч
+var isnewGame;
 /**********************************************************************************
  ***тоглогчидийн тоглосн оноог цуглуулах хувьсагч.
  *********************************************************************************/
@@ -19,22 +20,24 @@ var roundScore = 0;
 //Шооны зурагыг харуулахгүй
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-//шинээр эхлүүлэх
-newGame();
+
 //тоглоом шинээр эхлэх
+newGame();
 function newGame() {
+  //тоглоом эхэллээ гэдэг төлөв.
+  isnewGame = true;
   activePlayer = 0;
   Scorres = [0, 0];
   roundScore = 0;
   //дурын тоо өгөх
   diceNumber = Math.floor(Math.random() * 6) + 1;
+  //зураг түр зур хаах
+  diceDom.style.display = "none";
   //тоглоом бэлтгэх
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
   document.getElementById("current-1").textContent = "0";
   document.getElementById("current-0").textContent = "0";
-  //зураг түр зур хаах
-  diceDom.style.display = "none";
   //нэрийг буцаах
   document.getElementById("name-0").textContent = "Player 1";
   document.getElementById("name-1").textContent = "Player 2";
@@ -49,48 +52,57 @@ function newGame() {
  ***Дурийн тоо өгөх буюу Roll dice ажиллуулах anonymous функц болон callback функц.
  *********************************************************************************/
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  //Шооны дурын тоо өгнө.
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
-  //Шооны зургыг гаргаж өгнө.
-  diceDom.style.display = "block";
-  //Буусан санамсаргүй зургыг гаргаж ирнэ.
-  diceDom.src = "dice-" + diceNumber + ".png";
+  if (isnewGame) {
+    //Шооны дурын тоо өгнө.
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    //Шооны зургыг гаргаж өгнө.
+    diceDom.style.display = "block";
+    //Буусан санамсаргүй зургыг гаргаж ирнэ.
+    diceDom.src = "dice-" + diceNumber + ".png";
 
-  /**********************************************************************************
-   ***Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог шилжүүлнэ.
-   *********************************************************************************/
-  if (diceNumber !== 1) {
-    //1-ээс ялгаатай тоо буулаа. буусан тоог нэмж өгнө.
-    roundScore = roundScore + diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
+    /**********************************************************************************
+     ***Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог шилжүүлнэ.
+     *********************************************************************************/
+    if (diceNumber !== 1) {
+      //1-ээс ялгаатай тоо буулаа. буусан тоог нэмж өгнө.
+      roundScore = roundScore + diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      // if(activePlayer === 0){
+      //   activePlayer = 1
+      // }else{
+      //   activePlayer = 0
+      // }
+      switchToNextPlayer();
+    }
   } else {
-    // if(activePlayer === 0){
-    //   activePlayer = 1
-    // }else{
-    //   activePlayer = 0
-    // }
-    switchToNextPlayer();
   }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function() {
-  Scorres[activePlayer] = Scorres[activePlayer] + roundScore;
-  //Дэлгэц дээр оноог гаргаж өгнө.
-  document.getElementById("score-" + activePlayer).textContent =
-    Scorres[activePlayer];
-  //Уг тоглогч хожсон эсэхийг шалгах
-  if (Scorres[activePlayer] >= 10) {
-    //ялагч гэсэн текстийг нэрийх нь оронд гаргана
-    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
-  } else {
-    //ээлжийг солих
-    switchToNextPlayer();
+  if (isnewGame) {
+    Scorres[activePlayer] = Scorres[activePlayer] + roundScore;
+    //Дэлгэц дээр оноог гаргаж өгнө.
+    document.getElementById("score-" + activePlayer).textContent =
+      Scorres[activePlayer];
+    //Уг тоглогч хожсон эсэхийг шалгах
+    if (Scorres[activePlayer] >= 10) {
+      isnewGame = false;
+      //тоглоомыг дуусан төлөвт оруулна.
+      //ялагч гэсэн текстийг нэрийх нь оронд гаргана
+      document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+    } else {
+      //ээлжийг солих
+      switchToNextPlayer();
+    }
   }
 });
 
